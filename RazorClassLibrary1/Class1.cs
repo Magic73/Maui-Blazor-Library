052c0c866be262f06acb0ff38e5a6d0b6a04ae0c
Magic73 <magic73@gmail.com>
2023-06-21 22:34:39 +0200

    Add files via upload

---
+++ RazorClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using MudBlazor.Services;
+
+using RazorClassLibrary1.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary1
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddMudServices();
+
+            services.AddSingleton<WeatherForecastService>();
+
+            return services;
+        }
+    }
+}
